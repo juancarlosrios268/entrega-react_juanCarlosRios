4b82668e543a3e6ef279755b7153c90ece77f783
juan carlos rios <juancarlosriosr@gmail.com>
2024-09-27 14:47:27 -0500

    primer comit segunda entrega react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
+import CartWidget from './components/CartWidget'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <NavBar/>
+      <ItemListContainer greeting={"Hola! Bienvenidos a mi tienda "}/>
+    </>
+  )
+}
+
+export default App
